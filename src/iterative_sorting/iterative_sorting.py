1d39185250c7136c005aebdbe4dfe623130c639b
Mike Landers <landers.mike@gmail.com>
2019-04-29 15:43:43 -0700

    iterative sorting complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -5,7 +5,7 @@
 arr = [2, 5, 8, 1, 3, 6, 7, 4]
 
 
-def selection_sort(arr):
+def instertion_sort(arr):
     # loop through n-1 elements
     for i in range(len(arr)):
         x = i
@@ -17,28 +17,42 @@ def selection_sort(arr):
     return arr
 
 
+print(instertion_sort(arr))
+
+
+arr = [2, 5, 8, 1, 3, 6, 7, 4]
+
+
+def selection_sort(arr):
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        # TO-DO: find next smallest element
+        # (hint, can do in 3 loc)
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        # TO-DO: swap
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+
+    return arr
+
+
 print(selection_sort(arr))
 
 # TO-DO:  implement the Bubble Sort function below
+
 
 arr = [2, 5, 8, 1, 3, 6, 7, 4]
 
 
 def bubble_sort(arr):
-    for i in range(len(arr)):
-        x = i
-        temp = arr[i]
-        while i > 0:
-            if arr[x] < arr[x-1]:
-                arr[x] = arr[x-1]
-                x-+1
-                arr[x] = temp
-            elif arr[x] > arr[x+1]:
-                arr[x] = arr[x+1]
-                i +=1
-                arr[x]=temp
-            else
-                return arr
+    for i in range(0, len(arr)):
+        for j in range(0, len(arr)):
+            if arr[i] < arr[j]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
 
 
 print(bubble_sort(arr))
