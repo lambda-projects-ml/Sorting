77b063bd11dd2271e9d606b01b0958aa0d283fb6
Mike Landers <landers.mike@gmail.com>
2019-04-30 17:04:02 -0700

    merg sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -15,15 +15,11 @@ def quick_sort(arr, low, high):
         for i in range(low, high):
             # Move all elements smaller than pivot to its left-hand side. Move all elements larger than pivot to its right-hand side.
             if arr[i] < arr[pivot_index]:
-                temp = arr[pivot_index+1]
-                arr[pivot_index+1] = arr[i]
-                arr[i] = temp
-
+                arr[i], arr[pivot_index+1] = arr[pivot_index+1], arr[i]
                 # swap pivot with element on its right
-                temp = arr[pivot_index]
-                arr[pivot_index] = arr[pivot_index+1]
-                arr[pivot_index+1] = temp
-                pivot_index += 1
+                arr[pivot_index], arr[pivot_index +
+                                      1] = arr[pivot_index + 1], arr[pivot_index]
+
     # (recursive case) Recursively Quick Sort LHS and RHS until (base case) a side only contains a single element
     arr = quick_sort(arr, low, pivot_index)
 
@@ -32,15 +28,32 @@ def quick_sort(arr, low, high):
     return arr
 
 
-print(quick_sort(arr, 0, len(arr)))
+# print(quick_sort([1, 3, 5, 7, 2, 8, 9, 4, 6],
+#                  0, len([1, 3, 5, 3, 2, 8, 9, 5, 3])))
 
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
+array = [random.randint(0, 100) for i in range(0, 20)]
 
 
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    # TO-DO
+    ia = 0
+    ib = 0
+
+    for i in range(0, len(merged_arr)):
+        if len(arrA)-1 < ia:
+            merged_arr[i] = arrB[ib]
+            ib += 1
+        elif len(arrB)-1 < ib:
+            merged_arr[i] = arrA[ia]
+            ia += 1
+        elif arrA[ia] <= arrB[ib]:
+            merged_arr[i] = arrA[ia]
+            ia += 1
+        else:
+            merged_arr[i] = arrB[ib]
+            ib += 1
 
     return merged_arr
 
@@ -49,10 +62,20 @@ def merge(arrA, arrB):
 def merge_sort(arr):
     # TO-DO
 
+    if len(arr) > 1:
+        left = merge_sort(arr[: (len(arr)//2)])
+        right = merge_sort(arr[(len(arr)//2):])
+        arr = merge(left, right)
+
     return arr
 
 
+# print(array)
+print(merge_sort(arr))
+
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
